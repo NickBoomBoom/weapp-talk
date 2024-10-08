3f8d5c9c28539f0b272503bc2a98c8cc0b23eabf
q.chen <q.chen.self@outlook.com>
2024-10-08 17:54:18 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,63 +1,94 @@
 // index.js 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cGRhdGVkX2F0IjoiMjAyNC0xMC0wOFQwNDoyNTo0NS4wMDlaIiwiYWRkcmVzcyI6eyJjb3VudHJ5IjpudWxsLCJwb3N0YWxfY29kZSI6bnVsbCwicmVnaW9uIjpudWxsLCJmb3JtYXR0ZWQiOm51bGx9LCJwaG9uZV9udW1iZXJfdmVyaWZpZWQiOmZhbHNlLCJwaG9uZV9udW1iZXIiOm51bGwsImxvY2FsZSI6bnVsbCwiem9uZWluZm8iOm51bGwsImJpcnRoZGF0ZSI6bnVsbCwiZ2VuZGVyIjoiVSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJlbWFpbCI6ImNoZW5xaUBiaWdjcnVpc2UuY24iLCJ3ZWJzaXRlIjpudWxsLCJwaWN0dXJlIjoiaHR0cHM6Ly9maWxlcy5hdXRoaW5nLmNvL2F1dGhpbmctY29uc29sZS9kZWZhdWx0LXVzZXItYXZhdGFyLnBuZyIsInByb2ZpbGUiOm51bGwsInByZWZlcnJlZF91c2VybmFtZSI6bnVsbCwibmlja25hbWUiOm51bGwsIm1pZGRsZV9uYW1lIjpudWxsLCJmYW1pbHlfbmFtZSI6bnVsbCwiZ2l2ZW5fbmFtZSI6bnVsbCwibmFtZSI6bnVsbCwic3ViIjoiNjQwMDY2OGE5Zjg2NzExMGU5OGQ5NjE5IiwiZXh0ZXJuYWxfaWQiOm51bGwsInVuaW9uaWQiOm51bGwsInVzZXJuYW1lIjpudWxsLCJkYXRhIjp7InR5cGUiOiJ1c2VyIiwidXNlclBvb2xJZCI6IjYyMjgwOTk2ZWU3ZDJkMTQxNGI4NzBiMyIsImFwcElkIjoiNjIzMTUyNThhYjBhNDI1MDVhMGQ2YmI4IiwiaWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJ1c2VySWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJfaWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJwaG9uZSI6bnVsbCwiZW1haWwiOiJjaGVucWlAYmlnY3J1aXNlLmNuIiwidXNlcm5hbWUiOm51bGwsInVuaW9uaWQiOm51bGwsIm9wZW5pZCI6bnVsbCwiY2xpZW50SWQiOiI2MjI4MDk5NmVlN2QyZDE0MTRiODcwYjMifSwidXNlcnBvb2xfaWQiOiI2MjI4MDk5NmVlN2QyZDE0MTRiODcwYjMiLCJhdWQiOiI2MjMxNTI1OGFiMGE0MjUwNWEwZDZiYjgiLCJleHAiOjE3Mjk1NzQyODYsImlhdCI6MTcyODM2NDY4NiwiaXNzIjoiaHR0cHM6Ly9ob29rcy5hdXRoaW5nLmNuL29pZGMifQ.22FBc0rwk2QBvHGaWNhyolICM4HPm9wxmSTcp-_F_2U`
+const WEB_SOCKET_URL = 'ws://localhost:3000'
+const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cGRhdGVkX2F0IjoiMjAyNC0xMC0wOFQwNzo0NjoxMy44MDdaIiwiYWRkcmVzcyI6eyJjb3VudHJ5IjpudWxsLCJwb3N0YWxfY29kZSI6bnVsbCwicmVnaW9uIjpudWxsLCJmb3JtYXR0ZWQiOm51bGx9LCJwaG9uZV9udW1iZXJfdmVyaWZpZWQiOmZhbHNlLCJwaG9uZV9udW1iZXIiOm51bGwsImxvY2FsZSI6bnVsbCwiem9uZWluZm8iOm51bGwsImJpcnRoZGF0ZSI6bnVsbCwiZ2VuZGVyIjoiVSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJlbWFpbCI6ImNoZW5xaUBiaWdjcnVpc2UuY24iLCJ3ZWJzaXRlIjpudWxsLCJwaWN0dXJlIjoiaHR0cHM6Ly9maWxlcy5hdXRoaW5nLmNvL2F1dGhpbmctY29uc29sZS9kZWZhdWx0LXVzZXItYXZhdGFyLnBuZyIsInByb2ZpbGUiOm51bGwsInByZWZlcnJlZF91c2VybmFtZSI6bnVsbCwibmlja25hbWUiOm51bGwsIm1pZGRsZV9uYW1lIjpudWxsLCJmYW1pbHlfbmFtZSI6bnVsbCwiZ2l2ZW5fbmFtZSI6bnVsbCwibmFtZSI6bnVsbCwic3ViIjoiNjQwMDY2OGE5Zjg2NzExMGU5OGQ5NjE5IiwiZXh0ZXJuYWxfaWQiOm51bGwsInVuaW9uaWQiOm51bGwsInVzZXJuYW1lIjpudWxsLCJkYXRhIjp7InR5cGUiOiJ1c2VyIiwidXNlclBvb2xJZCI6IjYyMjgwOTk2ZWU3ZDJkMTQxNGI4NzBiMyIsImFwcElkIjoiNjIzMTUyNThhYjBhNDI1MDVhMGQ2YmI4IiwiaWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJ1c2VySWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJfaWQiOiI2NDAwNjY4YTlmODY3MTEwZTk4ZDk2MTkiLCJwaG9uZSI6bnVsbCwiZW1haWwiOiJjaGVucWlAYmlnY3J1aXNlLmNuIiwidXNlcm5hbWUiOm51bGwsInVuaW9uaWQiOm51bGwsIm9wZW5pZCI6bnVsbCwiY2xpZW50SWQiOiI2MjI4MDk5NmVlN2QyZDE0MTRiODcwYjMifSwidXNlcnBvb2xfaWQiOiI2MjI4MDk5NmVlN2QyZDE0MTRiODcwYjMiLCJhdWQiOiI2MjMxNTI1OGFiMGE0MjUwNWEwZDZiYjgiLCJleHAiOjE3Mjk1ODMyMzgsImlhdCI6MTcyODM3MzYzOCwiaXNzIjoiaHR0cHM6Ly9ob29rcy5hdXRoaW5nLmNuL29pZGMifQ.kySdvF3C4erhyRILgaBiGAp00agjT4IEMm5S0eASrgs`
 const recorderManager = wx.getRecorderManager();
 const fs = wx.getFileSystemManager();
- 
-const socket = wx.connectSocket({
-  url: `ws://localhost:5174/socket.io/?token=${token}`, 
-  success: (res) => {
-    console.log('链接websocket success', res)
-  },
-  fail: (err) => {
-    console.error('链接websocket fail', err)
-  }
-})
+
+
 Page({
+  socket: wx.connectSocket({
+    url: WEB_SOCKET_URL,
+  }),
   data: {
-    isIng: false
+    isInputIng: false,
+    list: [],
   },
   onLoad() {
+    wx.showLoading({
+      title: '连接中...',
+
+    })
     this.initSocket()
     this.initRecorder()
   },
-
-  onShow() {
-
+  onUnload() {
+    this.socket.close()
   },
+
   initSocket() {
-    socket.onOpen(res => {
-      console.log('socket onOpen', res)
+    console.log(5555, this.socket)
+    this.socket.onOpen(() => {
+      console.log('socket onOpen')
+      this.socket.send({
+        data: JSON.stringify({
+          event: "init",
+          data: {
+            token
+          }
+        }),
+        success: res => {
+          console.log('send init', res)
+        },
+        fail: err => console.error('send init fail', err)
+      })
     })
 
-    socket.onClose(res => {
-      console.log('socket onClose', res)
+    this.socket.onMessage((res) => {
+      const data = JSON.parse(res.data)
+      console.log('socket onMessage', data)
+      const {
+        command
+      } = data
+      const {
+        list
+      } = this.data
+      this.setData({
+        list: [...list, data]
+      })
+      if (command === 'init') {
+        wx.hideLoading()
+      } else if (command === 'chat') {
+        console.log(333)
+      }
     })
 
-    socket.onError(err => {
+    this.socket.onClose(() => {
+      console.log('socket onClose')
+    })
+
+    this.socket.onError(err => {
       console.log('socket onError', err)
     })
 
-    socket.onMessage((res) => {
-      console.log('socket onMessage', res)
-    })
+
   },
 
   initRecorder() {
     recorderManager.onStart(() => {
       console.log('recorder start')
       this.setData({
-        isIng: true
+        isInputIng: true
       })
     })
     recorderManager.onPause(() => {
       console.log('recorder pause')
       this.setData({
-        isIng: false
+        isInputIng: false
       })
     })
     recorderManager.onStop((res) => {
       this.setData({
-        isIng: false
+        isInputIng: false
       })
       console.log('recorder stop', res)
       fs.readFile({
@@ -75,7 +106,7 @@ Page({
         icon: 'none'
       })
       this.setData({
-        isIng: false
+        isInputIng: false
       })
     })
     recorderManager.onFrameRecorded((res) => {
@@ -93,7 +124,7 @@ Page({
   },
   handleStart() {
     this.setData({
-      isIng: true
+      isInputIng: true
     })
     const options = {
       duration: 600000, // 最大值 600000 (10分钟)
@@ -107,6 +138,8 @@ Page({
   },
   handleStop() {
     recorderManager.stop()
-
   },
+  reset() {
+
+  }
 })
